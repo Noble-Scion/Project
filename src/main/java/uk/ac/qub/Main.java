c0d03301d1c0c5a0d8fb5bb27c3ab22071ea2896
40295194 <khanson01@qub.ac.uk>
2024-11-20 11:57:45 +0000

    Snyk scan

--- src/main/java/uk/ac/qub/Main.java
+++ src/main/java/uk/ac/qub/Main.java
@@ -1,8 +1,23 @@
 package uk.ac.qub;
+import uk.ac.qub.AccountDetails.*;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world! (Hello?)");
+        UserAccountDAO userAccountDAO = new UserAccountDAO();
+
+        // Replace with the actual user ID you want to retrieve
+        int userId = 1;
+        UserAccount userAccount = userAccountDAO.getUserAccountById(userId);
+
+        if (userAccount != null) {
+            System.out.println("User ID: " + userAccount.getUserId());
+            System.out.println("Email: " + userAccount.getUsername());
+            System.out.println("Password: " + userAccount.getPassword());
+        } else {
+            System.out.println("User with ID " + userId + " not found.");
+        }
+
     }
 }
